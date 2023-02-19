cfd0258fc9874130d770ebd987dc37f59ea1af72
Luiz Freitas <luizsilfre@gmail.com>
2023-02-18 23:42:54 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Luiz Freitas, borda preta, cantando, com camisa cinza."
+    )
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Luiz Freitas, borda branca, cantando, com camisa cinza."
+    )
+  }
+}
